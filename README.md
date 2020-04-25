fa6259505e3f9a0a255a69dc752162acb5abcf76
Karen HY Tu <karenhoayitu@gmail.com>
2020-04-25 18:16:40 +0800

    Set up the project and remove unused code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TaiwanCovid19Traker
+# Covid19 Traker
+
+This is a react hook practice project and also aim to display the [open data](https://data.gov.tw/dataset/120711) from DATA.GOV.TW.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "black", height: "100vh", color: "white" }}
+      className="d-flex text-center justify-content-center">
+      <h1 >Coming Soon</h1>
+    </div>
+  );
+}
+
+export default App;
