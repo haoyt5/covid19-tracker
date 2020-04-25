3e64e4d9b16b89d37e7b1a24ec376824ce8f5c35
Karen HY Tu <karenhoayitu@gmail.com>
2020-04-25 18:17:41 +0800

    Updates

---
+++ precache-manifest.50183511878455bab365bea8bffec594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d025c2b886e56438b1470070487909",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "f4d2eec24d4dfe2cc518",
+    "url": "/covid19-tracker/static/css/main.f572b092.chunk.css"
+  },
+  {
+    "revision": "0d4a565c9aedf262f14b",
+    "url": "/covid19-tracker/static/js/2.287c4172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-tracker/static/js/2.287c4172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d2eec24d4dfe2cc518",
+    "url": "/covid19-tracker/static/js/main.178ec806.chunk.js"
+  },
+  {
+    "revision": "adbf302f45a1b1cd2e66",
+    "url": "/covid19-tracker/static/js/runtime-main.31d5ad05.js"
+  }
+]);
