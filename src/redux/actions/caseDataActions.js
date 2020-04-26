8f86d8d3ef74e9042300b8c73696c3214c1ab553
Karen HY Tu <karenhoayitu@gmail.com>
2020-04-26 15:36:10 +0800

    Convert the city and county from Mandarin to English

--- src/redux/actions/caseDataActions.js
+++ src/redux/actions/caseDataActions.js
@@ -22,7 +22,29 @@ const convertToEnglish = (string) =>{
                           .replace(/嚴重特殊傳染性肺炎/g,"SARS-CoV-2 Infection")
                           .replace(/女/g, "Female").replace(/男/g, "Male")
                           .replace(/是/g, "Y").replace(/否/g, "N")
-             
+                          .replace(/宜蘭縣/g,"Yilan County")
+                          .replace(/基隆市/g,"Keelung City")
+                          .replace(/台北市/g,"Taipei City")
+                          .replace(/新北市/g,"New Taipei City")
+                          .replace(/桃園市/g,"Taoyuan City")
+                          .replace(/新竹市/g,"Hsinchu City")
+                          .replace(/新竹縣/g,"Hsinchu County")
+                          .replace(/苗栗縣/g,"Miaoli County")
+                          .replace(/台中市/g,"Taichung City")
+                          .replace(/南投縣/g,"Nantou County")
+                          .replace(/彰化縣/g,"Changhua County")
+                          .replace(/雲林縣/g,"Yunlin County")
+                          .replace(/嘉義市/g,"Chiayi City")
+                          .replace(/嘉義縣/g,"Chiayi County")
+                          .replace(/台南市/g,"Tainan City")
+                          .replace(/高雄市/g,"Kaohsiung City")
+                          .replace(/屏東縣/g,"Pingtung County")
+                          .replace(/台東縣/g,"Taitung County")
+                          .replace(/花蓮縣/g,"Hualien County")
+                          .replace(/澎湖縣/g,"Penghu County")
+                          .replace(/金門縣/g,"Kinmen County")
+                          .replace(/連江縣/g,"Lienchiang County")
+
   result = newstring     
   return result;
 }
@@ -35,15 +57,14 @@ export const getCaseListData = () => {
           payload: attatchID(apiResult)
         });
    
-    // fetch(
-    //   "https://cors-anywhere.herokuapp.com/https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.json")
+    // fetch("https://cors-anywhere.herokuapp.com/https://od.cdc.gov.tw/eic/Day_Confirmation_Age_County_Gender_19CoV.json")
     //   .then(r => r.text())
     //   .then(data => {
-    //     const apiResult = JSON.parse(data);
+    //     const apiResult = JSON.parse(convertToEnglish(data));
     //     console.log("apiResult",apiResult);
     //     dispatch({
     //       type: constant.CASE_LIST_SUCCESS,
-    //       payload: apiResult
+    //       payload: attatchID(apiResult)
     //     });
     //   })
     //   .catch()
